3d14b4c9ebf852115c806f60af12983bf4e0436f
Sapna Sivakumar <sivakumarsapna@gmail.com>
2017-02-20 20:32:13 +0100

    removing unwanted third party files

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -4,7 +4,7 @@
 
   angular
     .module('repoFinder')
-    .constant('malarkey', malarkey)
-    .constant('moment', moment);
+    // .constant('malarkey', malarkey)
+    // .constant('moment', moment);
 
 })();
